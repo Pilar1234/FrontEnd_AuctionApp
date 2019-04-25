6ab7c71b7cb843806c62002804cf8c974ffd7236
Pilar1234 <Pilar1234@users.noreply.github.com>
2019-04-25 21:19:05 +0200

    added side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  w3_open() {
+    document.getElementById('main').style.marginLeft = '25%';
+    document.getElementById('mySidebar').style.width = '25%';
+    document.getElementById('mySidebar').style.display = 'block';
+    document.getElementById('openNav').style.display = 'none';
+  }
+
+   w3_close() {
+    document.getElementById('main').style.marginLeft = '0%';
+    document.getElementById('mySidebar').style.display = 'none';
+    document.getElementById('openNav').style.display = 'inline-block';
+  }
 }

4d9e0bbbbe2c112a7fecafc7e166b9af21fda370
Pilar1234 <Pilar1234@users.noreply.github.com>
2019-06-05 20:33:24 +0200

    added unit test for users-create component

--- src/app/users/users-create/users-create.component.spec.ts
+++ src/app/users/users-create/users-create.component.spec.ts
@@ -2,21 +2,15 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {UsersCreateComponent} from './users-create.component';
 import {ToastrModule} from 'ngx-toastr';
-import {HttpClientModule} from '@angular/common/http';
 import {UserFormValidationService} from '../users-shared/user-form-validation.service';
 import {SharedModule} from '../../shared/shared.module';
 import {UserService} from '../users-shared/user.service';
-import {RouterTestingModule} from '@angular/router/testing';
-import {Routes} from '@angular/router';
 import {UsersListComponent} from '../users-list/users-list.component';
+import {HttpClientModule} from '@angular/common/http';
 
 describe('UsersCreateComponent', () => {
     let component: UsersCreateComponent;
     let fixture: ComponentFixture<UsersCreateComponent>;
-    const routes: Routes = [
-        {path: 'user-list', component: UsersListComponent},
-        {path: 'user-create', component: UsersCreateComponent},
-    ];
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -25,7 +19,6 @@ describe('UsersCreateComponent', () => {
                 UsersListComponent
             ],
             imports: [
-                RouterTestingModule.withRoutes(routes),
                 ToastrModule.forRoot(),
                 HttpClientModule,
                 SharedModule
@@ -46,4 +39,62 @@ describe('UsersCreateComponent', () => {
     it('should create', () => {
         expect(component).toBeTruthy();
     });
+
+    it('should render title in a h2 tag', () => {
+        const compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelector('h2').textContent).toContain('Register user');
+    });
+
+    it('clear inputs', () => {
+        component.userForm.controls.login.setValue('test');
+        component.userForm.controls.firstName.setValue('test');
+        component.userForm.controls.secondName.setValue('test');
+        component.userForm.controls.email.setValue('test@gmail.com');
+        component.clearInputs();
+        expect(component.userForm.controls.login.value).toBe(null);
+        expect(component.userForm.controls.firstName.value).toBe(null);
+        expect(component.userForm.controls.secondName.value).toBe(null);
+        expect(component.userForm.controls.email.value).toBe(null);
+    });
+
+    it('should form be invalid when empty', () => {
+        expect(component.userForm.valid).toBeFalsy();
+    });
+
+    it('should form be valid when all fields are filed', () => {
+        component.userForm.controls.login.setValue('test');
+        component.userForm.controls.firstName.setValue('test');
+        component.userForm.controls.secondName.setValue('test');
+        component.userForm.controls.email.setValue('test@gmail.com');
+        expect(component.userForm.valid).toBeTruthy();
+    });
+
+    it('login field validity', () => {
+        const login = component.userForm.controls.login;
+        expect(login.errors.required).toBeTruthy();
+    });
+
+    it('firstName field validity', () => {
+        const firstName = component.userForm.controls.firstName;
+        expect(firstName.errors.required).toBeTruthy();
+    });
+
+    it('secondName field validity', () => {
+        const secondName = component.userForm.controls.secondName;
+        expect(secondName.errors.required).toBeTruthy();
+    });
+
+    describe('email field validity', () => {
+        it('email - required', () => {
+            const email = component.userForm.controls.email;
+            expect(email.errors.required).toBeTruthy();
+        });
+
+        it('email - bad pattern', () => {
+            const email = component.userForm.controls.email;
+            component.userForm.controls.email.setValue('test');
+            expect(email.errors.email).toBeTruthy();
+        });
+    });
+
 });
--- src/app/users/users-create/users-create.component.ts
+++ src/app/users/users-create/users-create.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {FormGroup} from '@angular/forms';
-import {ToastrService} from 'ngx-toastr';
 import {UserFormValidationService} from '../users-shared/user-form-validation.service';
 import {UserModel} from '../users-shared/user.model';
 import {UserService} from '../users-shared/user.service';
@@ -11,21 +10,21 @@ import {UserService} from '../users-shared/user.service';
     styleUrls: ['./users-create.component.css']
 })
 export class UsersCreateComponent implements OnInit {
-    private userForm: FormGroup;
+    userForm: FormGroup;
+    private title = 'Register user';
     private inputsArray: any[];
     private user = {} as UserModel;
 
-    constructor(private toastr: ToastrService,
-                private userFormValidation: UserFormValidationService,
+    constructor(private userFormValidation: UserFormValidationService,
                 private userService: UserService) {
     }
 
     ngOnInit() {
         this.inputsArray = [
-            { field: 'login', header: 'Login' },
-            { field: 'firstName', header: 'First Name' },
-            { field: 'secondName', header: 'Second Name' },
-            { field: 'email', header: 'Email' },
+            {field: 'login', header: 'Login'},
+            {field: 'firstName', header: 'First Name'},
+            {field: 'secondName', header: 'Second Name'},
+            {field: 'email', header: 'Email'},
         ];
         this.userForm = this.userFormValidation.initForm(this.user);
     }
@@ -35,7 +34,6 @@ export class UsersCreateComponent implements OnInit {
     }
 
     onSubmit() {
-        this.toastr.success('Uczestnik pomyślnie dodany');
         this.userService.addUser(this.userForm.value).subscribe(
             (response) => console.log(response)
         );
